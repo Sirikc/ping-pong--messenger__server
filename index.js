a959dd4493fb1acb2bcfd3935f8132472c51ca92
Sirikc <mishao090408@gmail.com>
2026-02-26 14:04:25 +0300

    2. add get for require to messageHistory

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const messageStory = []
+const messageHistory = [];
+
+app.get('/messageHistory', (req, res) => {
+  res.status(200).json({ messageHistory: messageHistory.slice(-req.query.messagesNum) });
+});
 
 app.post('/chat', (req, res) => {
   if (!req.body) {
     return res.status(400).json({ error: "message is required" });
   }
-  const newMessage = { id: messageStory.length + 1, message: req.body.message };
+  const newMessage = { id: messageHistory.length + 1, message: req.body.message };
   const arrMessage = newMessage.message.split('-');
 
-  messageStory.push(newMessage);
+  messageHistory.push(newMessage);
+  if (messageHistory.length > 50) {
+    messageHistory.shift();
+  }
 
   const response = arrMessage.map((item) => {
     if (item === "ping") return "pong";
